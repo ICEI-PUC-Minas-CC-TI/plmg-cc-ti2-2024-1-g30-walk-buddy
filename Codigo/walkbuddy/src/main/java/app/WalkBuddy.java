525b817eda6819cc13a0f6d9caa95b60c715262d
FelipeMichalaros <felipemichalaros@gmail.com>
2024-04-08 16:08:04 -0300

    gitignore

--- Codigo/walkbuddy/src/main/java/app/WalkBuddy.java
+++ Codigo/walkbuddy/src/main/java/app/WalkBuddy.java
@@ -7,7 +7,7 @@ import service.UsuarioService;
 public class WalkBuddy {
 	public static void main(String[] args) {
 		get("hello",(request, response)->"Hello!");
-		get("abc",(request, response)->"def");
+		get("abc",(request, response)->"defgh");
 		get("test",(request, response)->UsuarioService.inserir(request, response));
 	}
 }
