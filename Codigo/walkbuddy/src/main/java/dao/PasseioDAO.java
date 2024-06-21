16f582e8f4314ba328a58adf76fbb85ed75bc98e
João Paulo Vaz de Melo Gonçalves <144739103+JoaoVazMello@users.noreply.github.com>
2024-06-21 01:51:17 -0300

    Update PasseioDAO.java
    alteraçoes na estrutura

--- Codigo/walkbuddy/src/main/java/dao/PasseioDAO.java
+++ Codigo/walkbuddy/src/main/java/dao/PasseioDAO.java
@@ -48,7 +48,7 @@ public class PasseioDAO {
 		return status;
 	}
 	
-	public static String listar() {
+	public static String listar(int id) {
 		String lista = "erro";
 		String json;
 		try {
@@ -56,7 +56,7 @@ public class PasseioDAO {
 			Statement st = conexao.createStatement();
 			String sql = "SELECT passeio.id AS passeioid, passeio.estado, passeio.data, passeio.hora, pet.nome AS nomePet, usuario.nome AS nomePasseador "+
 				"FROM passeio JOIN passeio_pet ON passeio.id = passeio_pet.id_passeio "+
-				"JOIN pet ON pet.id = passeio_pet.id_pet LEFT JOIN usuario ON usuario.id = passeio.id_passeador;";
+				"JOIN pet ON pet.id = passeio_pet.id_pet LEFT JOIN usuario ON usuario.id = passeio.id_passeador WHERE pet.id_dono = " + id + ";";
 			ResultSet rs = st.executeQuery(sql);
 			lista = "[";
 			while(rs.next()) {	            	
@@ -67,6 +67,38 @@ public class PasseioDAO {
 	    		json += "\"hora\": \"" + rs.getString("hora") + "\",";
 	    		json += "\"nomePet\": \"" + rs.getString("nomePet") + "\",";
 	    		json += "\"nomePasseador\": \"" + rs.getString("nomePasseador") + "\"";
+	    		json += "}";
+	        	lista += json + ",";
+	        }
+			if(lista.charAt(lista.length()-1) == ',')
+				lista = lista.substring(0, lista.length()-1);
+			lista += "]";
+			st.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return lista;
+	}
+	
+	public static String listarPasseador(int id) {
+		String lista = "erro";
+		String json;
+		try {
+			Connection conexao = DAO.conectar();
+			Statement st = conexao.createStatement();
+			String sql = "SELECT passeio.id AS passeioid, passeio.estado, passeio.data, passeio.hora, pet.nome AS nomePet, usuario.nome AS nomeDono "+
+				"FROM passeio JOIN passeio_pet ON passeio.id = passeio_pet.id_passeio "+
+				"JOIN pet ON pet.id = passeio_pet.id_pet LEFT JOIN usuario ON usuario.id = pet.id_dono WHERE passeio.id_passeador = " + id + ";";
+			ResultSet rs = st.executeQuery(sql);
+			lista = "[";
+			while(rs.next()) {	            	
+	        	json = "{";
+	        	json += "\"id\": " + rs.getInt("passeioid") + ",";
+	    		json += "\"data\": \"" + rs.getString("data") + "\",";
+	    		json += "\"estado\": \"" + rs.getString("estado") + "\",";
+	    		json += "\"hora\": \"" + rs.getString("hora") + "\",";
+	    		json += "\"nomePet\": \"" + rs.getString("nomePet") + "\",";
+	    		json += "\"nomeDono\": \"" + rs.getString("nomeDono") + "\"";
 	    		json += "}";
 	        	lista += json + ",";
 	        }
