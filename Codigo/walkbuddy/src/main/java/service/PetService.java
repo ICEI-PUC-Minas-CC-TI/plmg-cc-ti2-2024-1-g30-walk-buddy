78e493d78f56f1ce559b9523797c4e59074cd77c
João Paulo Vaz de Melo Gonçalves <144739103+JoaoVazMello@users.noreply.github.com>
2024-06-21 02:15:38 -0300

    Update PetService.java
    correçao de bug

--- Codigo/walkbuddy/src/main/java/service/PetService.java
+++ Codigo/walkbuddy/src/main/java/service/PetService.java
@@ -5,19 +5,6 @@ import model.Pet;
 import spark.Request;
 import spark.Response;
 
-public class PetService {
-	
-	public static String inserir(Request request, Response response) {
-		String resultado = "erro";
-		boolean sucesso = false;
-		response.status(400);
-		Pet pet = nepackage service;
-
-import dao.PetDAO;
-import model.Pet;
-import spark.Request;
-import spark.Response;
-
 
 public class PetService {
 	
@@ -43,29 +30,6 @@ public class PetService {
 		String resultado = "erro";
 		int idDono = Integer.parseInt(request.queryParams("id_dono"));
 		resultado = PetDAO.listar(idDono);
-        response.status(200);
-		return resultado;
-	}
-
-}
-w Pet();
-		pet.nome = request.queryParams("nome");
-		pet.dataNascimento = request.queryParams("dataNascimento");
-		pet.raca = request.queryParams("raca");
-		pet.perfilComportamental = request.queryParams("perfilComportamental");
-		pet.idDono = Integer.parseInt(request.queryParams("idDono"));
-		//pet.foto = request.queryParams("foto");
-		sucesso = PetDAO.inserir(pet);
-		if(sucesso) {
-			resultado = "Pet " + pet.nome + " adicionado";
-            response.status(201);
-		}
-		return resultado;
-	}
-	
-	public static String listar(Request request, Response response) {
-		String resultado = "erro";
-		resultado = PetDAO.listar();
         response.status(200);
 		return resultado;
 	}
